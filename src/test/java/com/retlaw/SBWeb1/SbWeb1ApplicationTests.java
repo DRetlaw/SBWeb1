3427864629f0c149b0c1478e9a6c05ee6818974f
Walter Dsouza <walterdsouza@in.ibm.com>
2024-07-24 17:21:39 +0530

    initial commit

---
+++ src/test/java/com/retlaw/SBWeb1/SbWeb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retlaw.SBWeb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbWeb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
